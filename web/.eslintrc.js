247fa8522eb3a68cd290866663d05174bf5775df
Anhzf <36941633+anhzf@users.noreply.github.com>
2022-05-10 03:28:01 +0700

    feat: :tada: Setup vite vue-ts eslint tailwindcss

---
+++ web/.eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended'
+  ],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2021,
+  },
+  plugins: [
+    '@typescript-eslint',
+    'vue',
+  ],
+  rules: {
+    // ts
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+    // vue
+    'vue/component-name-in-template-casing': ['error', 'PascalCase', {
+      registeredComponentsOnly: true,
+      ignores: [],
+    }],
+
+    // js
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'func-names': 'off',
+    'max-len': ['warn', {
+      code: 150,
+      // will ignore everything inside html attrs
+      ignorePattern: '[\\w-]+="([^"]*)"',
+    }],
+    'no-unused-vars': 'off',
+  }
+}
