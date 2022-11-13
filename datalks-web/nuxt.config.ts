2488b52b625b619a4330c66dc5da418a34b86719
Anhzf <36941633+anhzf@users.noreply.github.com>
2022-11-14 02:18:46 +0700

    feat(datalks): :sparkles: Add favicons

--- datalks-web/nuxt.config.ts
+++ datalks-web/nuxt.config.ts
@@ -9,8 +9,16 @@ export default defineNuxtConfig({
   app: {
     head: {
       titleTemplate: '%s | Datalks UNS',
+      link: [
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+      ],
       meta: [
         { name: 'description', content: 'Website Resmi Pusat Informasi dan Publikasi Data BEM UNS Kementerian Riset dan Data BEM UNS 2022 Kabinet Aksata Swara' },
+        { name: 'msapplication-TileColor', content: '#da532c' },
+        { name: 'theme-color', content: '#ffffff' },
       ],
       htmlAttrs: {
         lang: 'id',
